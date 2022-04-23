1d85916abf9c52f38c4531c14c1ae6cd1a7e5a70
Austin1a2b <cavalier283@gmail.com>
2022-04-23 16:20:05 +0800

    U83 -  UsersTop view OK

---
+++ src/components/TopUserCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="col-3">
+    <a href="#">
+      <img
+        src="http://via.placeholder.com/300x300?text=No+Image"
+        width="140px"
+        height="140px"
+      />
+    </a>
+    <h2>{{ user.name }}</h2>
+    <span class="badge badge-secondary"
+      >追蹤人數：{{ user.FollowerCount }}</span
+    >
+    <p class="mt-3">
+      <button
+        type="button"
+        class="btn btn-danger"
+        v-if="user.isFollowed"
+        @click.prevent.stop="deleteFollowed"
+      >
+        取消追蹤
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        v-else
+        @click.prevent.stop="addFollowed"
+      >
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    initialuser: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: this.initialuser,
+    };
+  },
+  methods: {
+    addFollowed() {
+      console.log(this.user);
+      this.user = {
+        ...this.user,
+        isFollowed: true,
+      };
+    },
+    deleteFollowed() {
+      this.user = {
+        ...this.user,
+        isFollowed: false,
+      };
+    },
+  },
+};
+</script>
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -3,16 +3,77 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">美食達人</h1>
+    <hr />
+    <div class="row text-center">
+      <TopUserCard v-for="user in users" :key="user.id" :initialuser="user" />
+    </div>
   </div>
 </template>
 
 
 <script>
 import NavTabs from "../components/NavTabs.vue";
+import TopUserCard from "../components/TopUserCard.vue";
+
+const dummyUsers = [
+  {
+    id: 1,
+    name: "root",
+    email: "root@example.com",
+    password: "$2a$10$5ZuIWPJga1gF67fHHgww/uT1BJLXeHhvndOVI554btHhy.e4iRkWC",
+    isAdmin: true,
+    image: null,
+    createdAt: "2022-04-21T06:55:38.000Z",
+    updatedAt: "2022-04-21T06:55:38.000Z",
+    Followers: [],
+    FollowerCount: 0,
+    isFollowed: false,
+  },
+  {
+    id: 2,
+    name: "user1",
+    email: "user1@example.com",
+    password: "$2a$10$3Kgdc5ZwrYVooBuy.53ePeTBzUCAocqO.TK1OwUoQ.neIiBfZj4.y",
+    isAdmin: false,
+    image: null,
+    createdAt: "2022-04-21T06:55:38.000Z",
+    updatedAt: "2022-04-21T06:55:38.000Z",
+    Followers: [],
+    FollowerCount: 0,
+    isFollowed: false,
+  },
+  {
+    id: 3,
+    name: "user2",
+    email: "user2@example.com",
+    password: "$2a$10$8pGBE8rq2UKJ.InOgUMP1ekkJmR30W13BOuzlcmbwypi6twsBjGcW",
+    isAdmin: false,
+    image: null,
+    createdAt: "2022-04-21T06:55:38.000Z",
+    updatedAt: "2022-04-21T06:55:38.000Z",
+    Followers: [],
+    FollowerCount: 0,
+    isFollowed: false,
+  },
+];
 
 export default {
+  created() {
+    this.fetchUsers();
+  },
   components: {
     NavTabs: NavTabs,
+    TopUserCard: TopUserCard,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    fetchUsers() {
+      this.users = dummyUsers;
+    },
   },
 };
 </script>
